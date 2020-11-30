0ed417a745f74bea1d7ea07e167239b70cbcf5e4
aestrada611 <>
2020-11-30 09:11:09 -0800

    Finished to best of my ability

--- code-challenges.js
+++ code-challenges.js
@@ -9,6 +9,19 @@ var collections = ["purple", "blue", "green", "yellow", "pink"]
 
 
 
+//I couldnt get this one finished up
+
+
+
+const arrayDeleter = (array) => {
+  let arrayDeleter = array.shift()
+
+  return arrayDeleter
+}
+
+console.log(arrayDeleter(collections));
+
+
 
 
 // --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
@@ -18,9 +31,18 @@ var cubeAndSum1 = [2, 3, 4]
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+//must build out function that takes in array
+//the fucntion must be summed up and then cubbed
+const sumCubed = (array) => {
+  let reducer = array.reduce((a, b) => a + b, 0)
+  let cuber = reducer ** 3
+  return cuber
+}
 
 
 
+console.log(sumCubed(cubeAndSum1));
+console.log(sumCubed(cubeAndSum2));
 
 // --------------------3) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
@@ -29,9 +51,46 @@ var nums1 = [3, 56, 90, -8, 0, 23, 6]
 var nums2 = [109, 5, 9, -59, 8, 24]
 // Expected output: [-59, 109]
 
+//function takes in array
+const minMax = (array) => {
+  //let newArray = []
+  //have to sort in order to put in order
+  let arraySorter = array.sort(function(a,b){return a-b})
+  //must filter to only include the vlaues at the extremes
+  let arrayFilter = arraySorter.filter((value, index) => {
+    return index === 0 || index == arraySorter.length - 1
+  })
+return arrayFilter
+  //return arraySorter
+}
 
 
 
+//wanted to take another crack at it using an empty array and pushing into it.
+const minMax2 = (array) => {
+  let finalArray = []
+  let arraySorter2 = array.sort(function(a,b){return a-b})
+  let popper = arraySorter2.pop()
+  let pusher = finalArray.push(popper)
+  let shifter = arraySorter2.shift()
+  let unshifter = finalArray.unshift(shifter)
+  return finalArray
+
+}
+//need to push values from one array to the new array.
+//numbers are in order just need to know how to extract the two extrems
+//two possible ways are pop and shift but maybe use of index can help reutrn proper values
+
+
+
+
+
+console.log(minMax(nums1));
+console.log(minMax(nums2));
+
+
+console.log(minMax2(nums1));
+console.log(minMax2(nums2));
 
 // --------------------4) Create a function that takes in a string and returns a string with every other letter capitalized.
 
@@ -41,7 +100,27 @@ var testString2 = "jabberwocky"
 // Expected output: "jAbBeRwOcKy"
 
 
+//The function must take in strings
 
+const capitalized = (string) => {
+  //have to split the string into array
+  let stringSplitter = string.split("")
+  //map through array and return odd index values capitlaized
+  let mappedArray = stringSplitter.map((value,index) => {
+    if(index % 2 == 0){
+      return value
+    } else {
+      return value.toUpperCase()
+    }
+  })
+  //must join array
+  return mappedArray.join("")
+}
+//conditional statement looking at odd and even index numbers to return the capital letters
+//must break string down into an array in order to interact with each elment of the array
+
+console.log(capitalized(testString1));
+console.log(capitalized(testString2));
 
 
 // --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
@@ -49,3 +128,40 @@ var testString2 = "jabberwocky"
 var arr1 = [3, 7, 10, 5, 4, 3, 3]
 var arr2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+
+
+
+//make one long array using concat
+//this will help with all the manipulation happening in one array. need to find method that gets rid of duplicate values
+//have to set value
+
+const noDuplicates = (array1, array2) => {
+ let concatArray = array1.concat(array2)
+ //I knew it was going to most likley be a filter because it would have to be a shorter array or iterate thorught some other way but had hard time finding a way to only return first occurance of an element
+ let timesChecker = concatArray.filter((value, index) => {
+   return concatArray.indexOf(value) === index
+ })
+ return timesChecker
+}
+
+
+//This solution betllow is really cool but i dont really know why it worked. I found the solution online when trying to find out how to remove duplicates from an array.
+
+
+const noDuplicates2 = (array1, array2) => {
+ let concat2 = array1.concat(array2)
+ let noDup= [...new Set(concat2)]
+return noDup
+}
+
+
+
+
+// const noDuplicates = (array1, array2) => {
+//  let noDuplicatesArray = []
+//
+// }
+
+console.log(noDuplicates(arr1, arr2));
+console.log(noDuplicates2(arr1, arr2));
--- interview-questions.md
+++ interview-questions.md
@@ -7,49 +7,51 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Are Ruby methods accessors or mutators?
 
-  Your answer:
+  Your answer: Ruby methods can be both mutators and accessors.
 
-  Researched answer:
+  Researched answer:There are both types of methods in ruby. Mutators set the value of an attribute of an object while an accessor is a method that gets the value of the attribute.
 
 
 
 2. What is a block in Ruby?
 
-  Your answer:
+  Your answer: Block is the a piece of code that is enclosed with certain logic.
 
-  Researched answer:
+  Researched answer: A block is a chunk of code that will assign a name to a block. It is always enclosed within braces and blocks are invoke a from of function with the same name as that of the block.
 
 
 
 3. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: It means it does not have to be explicitly said. The return will still happen.
 
-  Researched answer:
+  Researched answer: An implicit return is one that will return the last line automatically so it is not necessary to use a return keyword at the end of the code block.
 
 
 
 4. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: A programing model based on objects. Don't really know much besides that.
 
-  Researched answer:
+  Researched answer: A programing paradigm based on objects. This entail writing procedures or functions that perform operations on the data. Objects will contain both data and functions
 
 
 
 5. What is the difference between a class and an object?
 
-  Your answer:
+  Your answer: Classes are the blueprints for objects.
 
-  Researched answer:
+  Researched answer:A class is a template for objects and also can describe object behavior. An object is a member of an 'instance' of a class. Object holds state in which all properties have values.
 
 
 
 6. STRETCH: What is Duck Typing?
 
-  Your answer:
+  Your answer: No clue! Quack!
 
-  Researched answer:
+  Researched answer: It is a term used in dynamic languages that do not have strong typing. It means you don't need a type in order to invoke an existing method on an object. If the method is defined you can invoke it. 
+
+  "The name comes from the phrase "If it looks like a duck and quacks like a duck, it's a duck".""
 
 
 
